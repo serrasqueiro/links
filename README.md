41f28fa72f7a0c81340c96a4cc79781a861845e3
Henrique Moreira <h@serrasqueiro.com>
2021-12-27 17:05:01 +0000

    Added curiositystream.com/

---
+++ README.md
@@ -0,0 +1,14 @@
+# [links](https://gist.github.com/serrasqueiro/e9a6299636e40ca20c370b26c4b17830)
+Set the appropriate path to *zson* python package
+- Windows,
+  + e.g. `set PYTHONPATH=../../../anaceo/clonadas/src/packages`
+- Linux,
+  + e.g. `export PYTHONPATH=$HOME/anaceo/clonadas/src/packages`
+
+Run links.py, python script:
+- `python links.py`
+
+# Update links package
+- `git clone git@gist.github.com:e9a6299636e40ca20c370b26c4b17830.git`
+- change links.json as you wish
+- `git commit -a -m "Update"; git push`
--- links.py
+++ links.py
@@ -18,6 +18,8 @@ BY_ORDER = (
     "!",
     "ted-talks=",
     "ted-talks-info",
+    "curiosity-stream=",
+    "curiosity-stream-info",
 )
 
 
@@ -37,26 +39,15 @@ def sample(fname:str, opts:dict) -> bool:
     if opts["re-write"]:
         is_ok = tbl.save(fname + "~")
         return is_ok
-    infos = build_infos(open(fname, "r", encoding=encoding).read(), fname, tbl)
-    refs = tbl.get_one("ted-talks-info")
-    assert refs
-    is_ok = tbl.index("ted-talks-info")
-    assert is_ok
-    key, talks = tbl.get_one_key("ted-talks")
+    infos = build_infos(open(fname, "r", encoding=encoding).read(), tbl)
     print("=" * 20)
-    for item in talks:
-        a_id, a_key, a_mark, a_title = item["Id"], item["Key"], item["Mark"], item["Title"]
-        if a_id <= 0:
-            continue
-        url = key[key.index("=")+1:].replace("$1", a_key)
-        who = get_who(tbl, a_id)
-        xtra = f"By: {who}\n" if who else ""
-        if a_mark is None:
-            a_mark = "-"
-        print(f"""
-<li>{a_id:<6} {a_mark:<20} <a href="{url}" _target="_blank">{a_title}</a>
-{xtra}</li>
-""")
+    subitems = []
+    for subitem in BY_ORDER:
+        if subitem.endswith("="):
+            subitem = subitem[:-1]
+            subitems.append((subitem, subitem + "-info"))
+    for subitem, subitem_info in subitems:
+        html_raw(tbl, "h2", item=subitem, info=subitem_info)
     print("=" * 20)
     same_content = infos["same-content"]
     if not same_content:
@@ -64,21 +55,56 @@ def sample(fname:str, opts:dict) -> bool:
     return same_content
 
 
-def get_who(tbl:IdTable, a_id:int) -> str:
+def html_raw(tbl, *args, **kwargs):
+    if not args:
+        h_level = "h2"
+    else:
+        h_level = args[0]
+    h_levels = (f"<{h_level}>", f"</{h_level}>")
+    item = kwargs["item"]
+    info = kwargs["info"]  # e.g. ted-talks-info
+    refs = tbl.get_one(info)
+    assert refs
+    is_ok = tbl.index(info)
+    assert is_ok
+    key, talks = tbl.get_one_key(item)
+    pre = f"\n<!-- {item} {len(talks)-1} item(s); info='{info}' -->\n"
+    print(f"{pre}{h_levels[0]}{item}{h_levels[1]}")
+    for item in talks:
+        a_id, a_key, a_mark, a_title = item["Id"], item["Key"], item["Mark"], item["Title"]
+        if a_id <= 0:
+            continue
+        url = key[key.index("=")+1:].replace("$1", a_key)
+        who = get_who(tbl, info, a_id)
+        xtra = f"By: {who}\n" if who else ""
+        if a_mark is None:
+            a_mark = "-"
+        print(f"""
+<li>{a_id:<6} {a_mark:<20} <a href="{url}" _target="_blank">{a_title}</a>
+{xtra}</li>
+""".rstrip())
+
+
+def get_who(tbl:IdTable, info:str, a_id:int) -> str:
+    if not info:
+        info = "ted-talks-info"
     try:
-        item = tbl.get_by_key("ted-talks-info", a_id)
+        item = tbl.get_by_key(info, a_id)
     except KeyError:
         item = None
     if not item:
         return ""
-    who = item["Speakers"]
+    who = item.get("Speakers")
+    if not who:
+        return ""
     return who
 
 
-def build_infos(data:str, fname:str, tbl) -> dict:
+def build_infos(data:str, tbl) -> dict:
+    astr = tbl.dump() + "\n"
     infos = {
-        "same-content": data == tbl.dump() + "\n",
-        "dump-size": len(tbl.dump() + "\n"),
+        "same-content": data == astr,
+        "dump-size": len(astr),
         "file-size": len(data),
     }
     return infos
